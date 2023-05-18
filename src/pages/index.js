e2be21f6f74fbc6dce4dc91e1ae31408965cbd8f
Alejandro González Reyes <alexglzrys@gmail.com>
2023-05-18 14:23:01 -0600

    frontend - listar tareas

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,55 @@
-import React from 'react'
+import axios from "axios";
+import Image from "next/image";
+import React from "react";
+import { Button, Container, Card, Grid } from "semantic-ui-react";
 
-const HomePage = () => {
+const HomePage = ({ tasks }) => {
+  // Componente a renderizar si no hay tareas
+  if (!tasks.length) return <Grid centered verticalAlign="middle" columns={1} style={{"min-height": "100vh"}}>
+    <Grid.Row>
+      <Grid.Column textAlign="center">
+        <h3>No hay tareas registradas</h3>
+        <Image src='https://michassi.com/assets/web//images/no_data_found.png' alt="No existen tareas" width={314} height={314} />
+        <div>
+          <Button primary>Registrar una tarea</Button>
+        </div>
+      </Grid.Column>
+    </Grid.Row>
+  </Grid>;
+
+  // Componente a renderizar cuando existen tareas
   return (
-    <div>HomePage</div>
-  )
-}
+    <Container>
+      <Card.Group itemsPerRow={4}>
+        {tasks.map((task) => (
+          <Card key={task._id}>
+            <Card.Content>
+              <Card.Header>{task.title}</Card.Header>
+              <p>{task.description}</p>
+            </Card.Content>
+            <Card.Content extra>
+              <Button color="green">Ver</Button>
+              <Button color="orange">Editar</Button>
+            </Card.Content>
+          </Card>
+        ))}
+      </Card.Group>
+    </Container>
+  );
+};
 
-export default HomePage
+// Solicitar información al servidor - SEO
+export const getServerSideProps = async (context) => {
+  // Consultar todas las tareas en la base de datos
+  // Solicitar información a la API de tareas
+  const { data: tasks } = await axios.get("http://localhost:3000/api/tasks");
+
+  // Enviar datos de las tareas a la vista mediante props
+  return {
+    props: {
+      tasks,
+    },
+  };
+};
+
+export default HomePage;
