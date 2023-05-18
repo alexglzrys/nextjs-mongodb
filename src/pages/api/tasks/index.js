a5bf8c501e0e5bc073059066b40f508c937f7d8a
Alejandro González Reyes <alexglzrys@gmail.com>
2023-05-18 12:33:23 -0600

    guardar tarea en base de datos

--- src/pages/api/tasks/index.js
+++ src/pages/api/tasks/index.js
@@ -3,15 +3,30 @@ import { dbConnect } from "@/utils/mongoose";
 
 // Controlador de peticiones HTTP para el recurso de tareas
 const TaskHandler = async(req, res) => {
+    // Recuperar el método HTTP de la petición y el cuerpo de la petición
+    const {method, body} = req;
+
     // Conectar con base de datos
     dbConnect();
     
-    switch(req.method) {
+    switch(method) {
         case 'GET':
-            // Consultar todas las tareas registradas en la colección tasks
-            const tasks = await Task.find();
-            // Devolver el listado de tareas como respuesta al cliente
-            return res.status(200).json(tasks);
+            try {
+                // Consultar todas las tareas registradas en la colección tasks
+                const tasks = await Task.find();
+                // Devolver el listado de tareas como respuesta al cliente
+                return res.status(200).json(tasks);
+            } catch (error) {
+                return res.status(500).json(error.message)
+            }
+        case 'POST':
+            try {
+                // Registrar nueva tarea, con base al cuerpo de la petición
+                const new_task = await Task.create(body);
+                return res.status(201).json(new_task)
+            } catch (error) {
+                return res.status(500).json(error.message)
+            }
         default:
             return res.status(400).json({message: 'Método HTTP no soportado', error: true })
     }
